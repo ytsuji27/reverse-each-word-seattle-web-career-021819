56979932c434af1e9e01fcaac86f65b0bcfc2f27
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-14 04:50:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   reverse_string = ""
   array.each do |word|
-    reverse_string << word.reverse
+    reverse_string << "#{word.reverse} "
   end
   reverse_string
 end
